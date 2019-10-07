9f0f3d70f36c8a6a4ac11c01087a8ee3efc68403
Abbyyuan <abbyyuan01@gmail.com>
2019-10-07 21:52:04 +1100

    add view user infomation

---
+++ WebContent/viewUser.jsp
@@ -0,0 +1,57 @@
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" import="java.util.*" import="domain.User"
+    import="java.text.SimpleDateFormat"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>View User</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<style type="text/css">
+body {
+  	padding: 2rem 10rem 2rem 10rem;
+  
+}
+.blog {
+  text-align: center;
+}
+</style>
+</head>
+<body>
+	 <form action="./listUser" method="get">
+    	<!-- <div class="button">
+          <button type="submit" class="btn btn-info">List all users</button>
+        </div> -->
+    
+     <br><br>
+    <% List<User> users = (List<User>)request.getAttribute("users"); %>
+    <table class="table">
+	  <thead>
+	    <tr>
+	      <th scope="col">Id</th>
+	      <th scope="col">FirstName</th>
+	      <th scope="col">LastName</th>
+	      <th scope="col">Email</th>
+	      <th scope="col">Password</th>
+	    </tr>
+	  </thead>
+  <% if (users != null) { %>
+    <% for (User user : users) { %>
+	  <tbody>
+	    <tr>
+	      <th scope="row"><%=user.getId()%></th>
+	      <td><%=user.getId()%></td>
+	      <td><%=user.getFirstName()%></td>
+	      <td><%=user.getFirstName()%></td>
+	      <td><%=user.getEmail()%></td>
+	      <td><%=user.getPassword()%></td>
+	    </tr>
+	  </tbody>  	
+     <%} %>
+	 <%} %>
+	</table>
+</form>
+
+</body>
+</html>
---
+++ src/servlet/ListUserServlet.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.User;
+import domain.UserMapper;
+
+/**
+ * Servlet implementation class ListUserServlet
+ */
+@WebServlet("/listUser")
+public class ListUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListUserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		List<User> users = new ArrayList<User>();
+		 
+		users = UserMapper.findAllUsers();
+		
+		request.setAttribute("users", users);
+		RequestDispatcher rd = request.getRequestDispatcher("viewUser.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
